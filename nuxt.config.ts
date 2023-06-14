7114fad5304f051719d0e0cc88bf195a75109db2
Anthony Basang <anthony.basang@ecloudvalley.com>
2023-06-15 03:30:03 +0800

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      "postcss-nested": {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
 })
