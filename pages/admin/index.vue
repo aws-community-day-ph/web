35b2277967315e0ec610a18b6c2b4ae110bd0f08
rn-mnsl <manansalarin@gmail.com>
2023-07-10 19:03:02 +0800

    added upload container for image

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -298,6 +298,37 @@ h<template>
             </div>
         </div>
         </div>
+        <!-- Upload container-->
+        <div class="flex justify-center">
+          <div class="w-[1030px] h-[470px] flex flex-col items-center justify-center bg-[white] rounded-lg py-2 mt-6 drop-shadow-xl">
+            <div class="w-[630px] h-[374px] flex flex-col bg-[#37475A] rounded-lg py-2 mt-1">
+              <h3 class="font-semibold text-white text-xl justify-normal ml-7 mt-4">Upload your image:   
+              </h3>
+              <div class="w-[590px] h-[280px] flex flex-col items-center justify-center bg-[white] py-1 mt-6 ml-5">
+                <img
+                  src="@/assets/icons/image.svg"
+                  class="w-[67px] h-[53px] px-1 drop-shadow-lgr"
+                />
+                <h3 class="font-semibold text-[#37475A] text-xl items-center justify-center">Drag & Drop   
+                </h3>
+                <h3 class="font-semibold text-[#37475A] text-sm items-center justify-center">or   
+                </h3>
+                <button
+                @click="BrowseFile"
+                class="bg-[#146eb4] w-[135px] h-[36px] rounded-lg text-white flex items-center justify-center hover:bg-[#264873] focus:bg-[#146eb4] duration-500"
+                >
+                Browse File
+                </button>
+              </div>
+            </div>
+            <button
+                @click="Upload"
+                class="bg-[#FEBD69] w-[145px] h-[46px] text-xl font-semibold rounded-lg text-black flex items-center justify-center hover:bg-[#FF9900] focus:bg-[#FEBD69] duration-500 mt-8"
+                >
+                UPLOAD
+                </button>
+          </div> 
+        </div>
       </div>
     </div>
   </div>
@@ -315,6 +346,8 @@ export default {
       selectedRequestIndex: -1,
       activeRequestID: null,
       DeleteRequest: false,
+      BrowseFile: [], //no code 
+      Upload: [], //no code
     };
   },
   computed: {
