4292c04a4dd6e40ac9785f81001dadc8e07430fe
Anthony Basang <anthony.basang@ecloudvalley.com>
2023-07-28 23:50:26 +0800

    preview

--- pages/preview.vue
+++ pages/preview.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="photo-editor">
     <div
+      ref="photoTemplateRef"
       class="photo-template bg-cover"
       :style="{
         width: `${PHOTO_TEMPLATE_WIDTH}px`,
@@ -8,7 +9,10 @@
         backgroundImage: `url(${backgroundImage})`,
       }"
     >
-      <div class="uploaded-image-container">
+      <div
+        class="uploaded-image-container"
+        :class="[{ 'border-2 border-dashed border-gray-300': !uploadedImage }]"
+      >
         <div v-if="!uploadedImage" class="upload-placeholder">
           <input type="file" @change="handleImageUpload" />
         </div>
@@ -19,80 +23,117 @@
           alt="Uploaded Image"
         />
       </div>
-   
     </div>
     <div class="buttons">
-        <button @click="saveImage">Save</button>
-        <button @click="resetImage">Reset</button>
-      </div>
+      <button @click="saveImage">Save</button>
+      <button @click="resetImage">Reset</button>
+    </div>
   </div>
 </template>
   
-<script setup>
+<script>
 import { ref } from "vue";
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
+export default {
+  setup() {
+    const PHOTO_TEMPLATE_WIDTH = ref(1080);
+    const PHOTO_TEMPLATE_HEIGHT = ref(700);
+    const UPLOADED_IMAGE_WIDTH = ref(840);
+    const UPLOADED_IMAGE_HEIGHT = ref(575);
+    const photoTemplateRef = ref(null);
 
-const PHOTO_TEMPLATE_WIDTH = 1080;
-const PHOTO_TEMPLATE_HEIGHT = 700;
-const UPLOADED_IMAGE_WIDTH = 840;
-const UPLOADED_IMAGE_HEIGHT = 575;
+    const backgroundImage = ref("../assets/template/photo_booth_template.png");
 
-const backgroundImage = ref("../assets/template/photo_booth_template.png");
+    const uploadedImage = ref(null);
 
-const uploadedImage = ref(null);
+    const handleImageUpload = (event) => {
+      const file = event.target.files[0];
+      const reader = new FileReader();
 
-const handleImageUpload = (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = new Image();
+        img.src = e.target.result;
 
-  reader.onload = (e) => {
-    const img = new Image();
-    img.src = e.target.result;
+        img.onload = () => {
+          const { width, height } = scaleImageToFitTemplate(
+            img.width,
+            img.height
+          );
 
-    img.onload = () => {
-      const { width, height } = scaleImageToFitTemplate(img.width, img.height);
+          const canvas = document.createElement("canvas");
+          canvas.width = UPLOADED_IMAGE_WIDTH.value;
+          canvas.height = UPLOADED_IMAGE_HEIGHT.value;
+          const ctx = canvas.getContext("2d");
 
-      const canvas = document.createElement("canvas");
-      canvas.width = UPLOADED_IMAGE_WIDTH;
-      canvas.height = UPLOADED_IMAGE_HEIGHT;
-      const ctx = canvas.getContext("2d");
+          // Calculate the position to center the image within the canvas
+          const x = (UPLOADED_IMAGE_WIDTH.value - width) / 2;
+          const y = (UPLOADED_IMAGE_HEIGHT.value - height) / 2;
 
-      // Calculate the position to center the image within the canvas
-      const x = (UPLOADED_IMAGE_WIDTH - width) / 2;
-      const y = (UPLOADED_IMAGE_HEIGHT - height) / 2;
+          ctx.drawImage(img, x, y, width, height);
 
-      ctx.drawImage(img, x, y, width, height);
+          uploadedImage.value = canvas.toDataURL("image/jpeg");
+        };
+      };
 
-      uploadedImage.value = canvas.toDataURL("image/jpeg");
+      reader.readAsDataURL(file);
     };
-  };
 
-  reader.readAsDataURL(file);
-};
+    const scaleImageToFitTemplate = (imgWidth, imgHeight) => {
+      const templateAspectRatio =
+        PHOTO_TEMPLATE_WIDTH.value / PHOTO_TEMPLATE_HEIGHT.value;
+      const imageAspectRatio = imgWidth / imgHeight;
 
-const scaleImageToFitTemplate = (imgWidth, imgHeight) => {
-  const templateAspectRatio = PHOTO_TEMPLATE_WIDTH / PHOTO_TEMPLATE_HEIGHT;
-  const imageAspectRatio = imgWidth / imgHeight;
+      let newWidth, newHeight;
 
-  let newWidth, newHeight;
+      if (templateAspectRatio > imageAspectRatio) {
+        newWidth = UPLOADED_IMAGE_WIDTH.value;
+        newHeight = UPLOADED_IMAGE_WIDTH.value / imageAspectRatio;
+      } else {
+        newHeight = UPLOADED_IMAGE_HEIGHT.value;
+        newWidth = UPLOADED_IMAGE_HEIGHT.value * imageAspectRatio;
+      }
 
-  if (templateAspectRatio > imageAspectRatio) {
-    newWidth = UPLOADED_IMAGE_WIDTH;
-    newHeight = UPLOADED_IMAGE_WIDTH / imageAspectRatio;
-  } else {
-    newHeight = UPLOADED_IMAGE_HEIGHT;
-    newWidth = UPLOADED_IMAGE_HEIGHT * imageAspectRatio;
-  }
+      return { width: newWidth, height: newHeight };
+    };
 
-  return { width: newWidth, height: newHeight };
-};
+    const saveImage = () => {
+      console.log("hellp");
+      const photoTemplate = photoTemplateRef.value;
+      console.log(photoTemplate);
+      if (!photoTemplate) return;
 
-const saveImage = () => {
-  // Logic to save the image (e.g., send it to the server).
-  // Replace this with your implementation.
-};
+      // Convert the photo template to an image
+      htmlToImage
+        .toPng(photoTemplate)
+        .then(function (dataUrl) {
+          const link = document.createElement("a");
+          link.href = dataUrl;
+          link.download = "my-node.png";
+          link.click();
+        })
+        .catch(function (error) {
+          console.error("oops, something went wrong!", error);
+        });
+    };
 
-const resetImage = () => {
-  uploadedImage.value = null;
+    const resetImage = () => {
+      uploadedImage.value = null;
+    };
+
+    return {
+      saveImage,
+      resetImage,
+      handleImageUpload,
+      PHOTO_TEMPLATE_WIDTH,
+      PHOTO_TEMPLATE_HEIGHT,
+      UPLOADED_IMAGE_WIDTH,
+      UPLOADED_IMAGE_HEIGHT,
+      photoTemplateRef,
+      backgroundImage,
+      uploadedImage,
+    };
+  },
 };
 </script>
   
@@ -114,8 +155,8 @@ const resetImage = () => {
 }
 
 .uploaded-image-container {
-  @apply border-2 border-dashed border-gray-300 -mt-[84px];
-  @apply w-[867px] h-[586px];
+  @apply -mt-[81px];
+  @apply w-[867px] h-[589px];
 }
 
 .uploaded-image {
